1f52cb254d39802ca0b28733879e431a52d50acb
Ben Donnelly <ben_donnelly@intergral.com>
2023-04-17 12:43:11 +0100

    initial commit of deep-datasource

---
+++ pkg/plugin/datasource.go
@@ -0,0 +1,159 @@
+package plugin
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/httpclient"
+	"io"
+	"net/http"
+	"time"
+
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
+	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
+	"github.com/grafana/grafana-plugin-sdk-go/data"
+)
+
+// Make sure DeepDatasource implements required interfaces. This is important to do
+// since otherwise we will only get a not implemented error response from plugin in
+// runtime. In this example datasource instance implements backend.QueryDataHandler,
+// backend.CheckHealthHandler interfaces. Plugin should not implement all these
+// interfaces- only those which are required for a particular task.
+var (
+	_ backend.QueryDataHandler      = (*DeepDatasource)(nil)
+	_ backend.CheckHealthHandler    = (*DeepDatasource)(nil)
+	_ instancemgmt.InstanceDisposer = (*DeepDatasource)(nil)
+)
+
+// NewDeepDatasource creates a new datasource instance.
+func NewDeepDatasource(settings backend.DataSourceInstanceSettings) (instancemgmt.Instance, error) {
+	client, err := buildHttpClient(settings)
+	if err != nil {
+		return nil, err
+	}
+	return &DeepDatasource{
+		client: client,
+	}, nil
+}
+
+func buildHttpClient(settings backend.DataSourceInstanceSettings) (*http.Client, error) {
+	return httpclient.New()
+}
+
+// DeepDatasource is an example datasource which can respond to data queries, reports
+// its health and has streaming skills.
+type DeepDatasource struct {
+	client *http.Client
+}
+
+// Dispose here tells plugin SDK that plugin wants to clean up resources when a new instance
+// created. As soon as datasource settings change detected by SDK old datasource instance will
+// be disposed and a new one will be created using NewSampleDatasource factory function.
+func (d *DeepDatasource) Dispose() {
+	// Clean up datasource instance resources.
+}
+
+// QueryData handles multiple queries and returns multiple responses.
+// req contains the queries []DataQuery (where each query contains RefID as a unique identifier).
+// The QueryDataResponse contains a map of RefID to the response for each query, and each response
+// contains Frames ([]*Frame).
+func (d *DeepDatasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
+	// create response struct
+	response := backend.NewQueryDataResponse()
+
+	// loop over queries and execute them individually.
+	for _, q := range req.Queries {
+		res := d.query(ctx, req.PluginContext, q)
+
+		// save the response in a hashmap
+		// based on with RefID as identifier
+		response.Responses[q.RefID] = res
+	}
+
+	return response, nil
+}
+
+type queryModel struct{}
+
+func (d *DeepDatasource) query(_ context.Context, pCtx backend.PluginContext, query backend.DataQuery) backend.DataResponse {
+	var response backend.DataResponse
+
+	// Unmarshal the JSON into our queryModel.
+	var qm queryModel
+
+	err := json.Unmarshal(query.JSON, &qm)
+	if err != nil {
+		return backend.ErrDataResponse(backend.StatusBadRequest, fmt.Sprintf("json unmarshal: %v", err.Error()))
+	}
+
+	// create data frame response.
+	// For an overview on data frames and how grafana handles them:
+	// https://grafana.com/docs/grafana/latest/developers/plugins/data-frames/
+	frame := data.NewFrame("response")
+
+	// add fields.
+	frame.Fields = append(frame.Fields,
+		data.NewField("time", nil, []time.Time{query.TimeRange.From, query.TimeRange.To}),
+		data.NewField("values", nil, []int64{10, 20}),
+	)
+
+	// add the frames to the response.
+	response.Frames = append(response.Frames, frame)
+
+	return response
+}
+
+// CheckHealth handles health checks sent from Grafana to the plugin.
+// The main use case for these health checks is the test button on the
+// datasource configuration page which allows users to verify that
+// a datasource is working as expected.
+func (d *DeepDatasource) CheckHealth(_ context.Context, req *backend.CheckHealthRequest) (*backend.CheckHealthResult, error) {
+	url := fmt.Sprintf("%v/api/echo", req.PluginContext.DataSourceInstanceSettings.URL)
+
+	request, err := http.NewRequest(http.MethodGet, url, nil)
+
+	if err != nil {
+		return &backend.CheckHealthResult{
+			Status:  backend.HealthStatusError,
+			Message: fmt.Sprintf("%v", err),
+		}, nil
+	}
+
+	response, err := d.client.Do(request)
+
+	if err != nil {
+		return &backend.CheckHealthResult{
+			Status:  backend.HealthStatusError,
+			Message: fmt.Sprintf("%v", err),
+		}, nil
+	}
+
+	if response.StatusCode != 200 {
+		return &backend.CheckHealthResult{
+			Status:  backend.HealthStatusError,
+			Message: fmt.Sprintf("Status code %v returned", response.StatusCode),
+		}, nil
+	}
+
+	defer response.Body.Close()
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return &backend.CheckHealthResult{
+			Status:  backend.HealthStatusError,
+			Message: "Could not parse response body",
+		}, nil
+	}
+
+	responseBody := string(body)
+	if responseBody != "echo\n" {
+		return &backend.CheckHealthResult{
+			Status:  backend.HealthStatusError,
+			Message: "Unexpected response from DEEP. Is this a DEEP instance?",
+		}, nil
+	}
+
+	return &backend.CheckHealthResult{
+		Status:  backend.HealthStatusOk,
+		Message: "Data source is working",
+	}, nil
+}
---
+++ src/components/ConfigEditor.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {DataSourceHttpSettings} from '@grafana/ui';
+import {DataSourcePluginOptionsEditorProps} from '@grafana/data';
+import {MyDataSourceOptions} from '../types';
+
+interface Props extends DataSourcePluginOptionsEditorProps<MyDataSourceOptions> {
+}
+
+export function ConfigEditor({onOptionsChange, options}: Props) {
+
+    return (
+        <div className="gf-form-group">
+            <DataSourceHttpSettings
+                defaultUrl="http://deep"
+                dataSourceConfig={options}
+                showAccessOptions={false}
+                onChange={onOptionsChange}
+            />
+        </div>
+    );
+}
