7345e2b65b901f4bc54af8c327cb095ef20a573a
Ben Donnelly <b.w.donnelly1@googlemail.com>
2024-02-21 13:45:13 +0000

    chore(update): update project to latest plugin template and dep versions

--- cypress/integration/01-smoke.spec.ts
+++ cypress/integration/01-smoke.spec.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023  Intergral GmbH
+ * Copyright (C) 2024  Intergral GmbH
  *
  *     This program is free software: you can redistribute it and/or modify
  *     it under the terms of the GNU Affero General Public License as published by
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/intergral/grafana-deep-datasource
 
-go 1.20
+go 1.22
 
 require (
 	github.com/go-kit/log v0.2.1
